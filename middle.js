fc164e305447a740ea4285393ad8f35af166fabf
github4maninder <maninder.email@gmail.com>
2021-10-27 18:44:01 -0400

    add countOnly.js file in repo

--- middle.js
+++ middle.js
@@ -40,12 +40,11 @@ const middle = (array) => {
     return [array[Math.floor(array.length / 2)]];
   }
 };
-
 // output test code
-console.log(middle([1]));
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5]));
+// console.log(middle([1]));
+// console.log(middle([1, 2]));
+// console.log(middle([1, 2, 3]));
+//console.log(middle([1, 2, 3, 4, 5]));
 console.log(middle([1, 2, 3, 4]));
 console.log(middle([1, 2, 3, 4, 5, 6]));
 assertArraysEqual(middle([1]),[]);
