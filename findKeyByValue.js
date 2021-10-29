54d18f73f5e91c9270f41bdd6ad23879c2d8be70
github4maninder <maninder.email@gmail.com>
2021-10-29 14:40:08 -0400

    add findKeyByValue file"

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = (actual, expected) =>{
+  if (actual === expected) {
+    console.log(`✅✅✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = (movies, whatGenre) => {
+  const genre = Object.keys(movies);
+  for (let movie of genre)  {
+    if (movies[movie] === whatGenre) {
+      return movie;
+    }
+  }
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
