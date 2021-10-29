e0043466bdb9cdaad114254a78a4b2050316be52
github4maninder <maninder.email@gmail.com>
2021-10-29 14:17:24 -0400

    add update file

--- eqObject.js
+++ eqObject.js
@@ -22,6 +22,7 @@ const eqArrays = (index1, index2) => {
   }
 };
 
+
 const eqObjects = function(object1, object2) {
   const array1 = Object.keys(object1);
   const array2 = Object.keys(object2);
@@ -42,3 +43,19 @@ const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 eqObjects(ab, ba);  // => true
 
+const abc = { a: "1", b: "2", c: "3" };
+eqObjects(ab, abc); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+eqObjects(cd, dc); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+eqObjects(cd, cd2); // => false
+
+// TEST CODE
+
+assertEqual(eqObjects(ab, ba), true);
+assertEqual(eqObjects(ab, abc), false);
+assertEqual(eqObjects(cd, dc), true);
+assertEqual(eqObjects(cd, cd2), false);
