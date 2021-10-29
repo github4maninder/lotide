c28d7196baa48dec32ddf12a6f1eb609ad8ac163
github4maninder <maninder.email@gmail.com>
2021-10-29 14:57:50 -0400

    add file in repo

---
+++ assertObjectsEqual.js
@@ -0,0 +1,56 @@
+ 
+const assertEqual = (actual, expected) =>{
+  if (actual === expected) {
+    console.log(`✅✅✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (index1, index2) => {
+ 
+  if (index1.length !== index2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < index1.length; i++) {
+      if (index1[i] !== index2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const eqObjects = function(object1, object2) {
+  const array1 = Object.keys(object1);
+  const array2 = Object.keys(object2);
+  if ((array1.length) === (array2.length)) {
+    for (let key of array1) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  return false;
+};
+
+// FUNCTION IMPLEMENTATION
+
+
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+
+  if (!eqObjects(actual, expected))  {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  }
+};
+
+const ab = { a: "4", b: "2"};
+const ba = { b: "2", a: "1"};
+assertObjectsEqual((ab,ba),false)
