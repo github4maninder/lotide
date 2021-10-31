c6cc0429221f43e751e8a84f1d2bcfd61282edec
github4maninder <maninder.email@gmail.com>
2021-10-31 19:07:42 -0400

    add comments

--- countLetters.js
+++ countLetters.js
@@ -9,7 +9,7 @@ const assertEqual = (actual, expected) =>{
 const countLetters = function (sentence) {
   const result = {};
   for (let letter of sentence) {
-    if (letter !== " ") {   // letter is initinalized
+    if (letter !== " ") {   // letter is initinalized 
       if (result[letter]) {   //letter is declared as the key to result obj if true
         result[letter] += 1;  // counting the key and adding to the value of the objects
       } else {
@@ -20,9 +20,10 @@ const countLetters = function (sentence) {
   return result;
 };
 
-const result1 = countLetters("My name is Maninder");
+const result1 = countLetters("My name is Maninder"); /* the sentence will be converted to object with key as the M,a,n,i,d,e,r and the values as 1,2,2,1,1,2,1 (count of the letters in sentence*/ 
 
 assertEqual(result1["n"], 3);
 assertEqual(result1["a"], 2);
 assertEqual(result1["o"], undefined);
-assertEqual(result1["i"], undefined);
+assertEqual(result1["i"], 2);
+assertEqual(result1["q"], 1);
--- tail.js
+++ tail.js
@@ -5,22 +5,12 @@ const assertEqual = (actual, expected) =>{
     console.log(`✅✅✅ ${actual} === ${expected}`);
   }
 };
-
-
 // Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-
-
+const result = ["Yo Yo", "Lighthouse", "Labs"];
 const tail = () => {
-  let newArr = words.slice(1);
+  let newArr = result.slice(1);
   return newArr;
 }
-
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
-
+tail(result); // no need to capture the return value since we are not checking it
+assertEqual(result.length, 3);
 assertEqual(String(tail(["Hello", "Lighthouse", "Labs"])), String(["Lighthouse", "Labs"]));
-assertEqual(String(tail([])), String([]));
-assertEqual(String(tail([1,2,3,4])), String([2,3,4]));
-assertEqual(String(tail([1])), String([]));
-assertEqual(String(tail([1,2,3,4,5])), String([1,2,3,4,5]));
