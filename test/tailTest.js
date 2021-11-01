1d4ef9949691ac39b830054c4d5cd889fba81066
github4maninder <maninder.email@gmail.com>
2021-11-01 19:17:24 -0400

    add tailTest with m and c test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,14 @@
-const assertEqual = require('../src/assertEqual');
-const tail = require('../src/tail')
+const assert = require('chai').assert;
+const tail   = require('../src/tail');
 
-assertEqual(String(tail(["Hello", "Lighthouse", "Labs"])), String(["Lighthouse", "Labs"]));
+describe("#tail", () => {
+  it("returns 2 for [1, 2]", () => {
+    assert.deepEqual(tail([1, 2]), [2]);
+  });
+  it("returns ['6','7'] for ['5','6','7']", () => {
+    assert.deepEqual(tail(['5','6','7']), ['6','7']); 
+  });
+  it("returns ['lighthouse','labs'] for ['hello','lighthouse','labs']", () => {
+    assert.deepEqual(tail(['hello','lighthouse','labs']), ['lighthouse','labs']); 
+  });
+});
